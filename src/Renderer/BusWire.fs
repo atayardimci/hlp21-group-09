0908010f48dbfff72d028d20cf78035a78be9558
kleze20 <68337035+kleze20@users.noreply.github.com>
2021-03-14 17:57:35 +0000

    Update BusWire.fs

--- src/Renderer/BusWire.fs
+++ src/Renderer/BusWire.fs
@@ -26,7 +26,8 @@ type Wire = {
     Selected: bool
     BusWidth: int
     relativPositions: XYPos list
-    PrevPositions: XYPos list// create a list of boundingboxes
+    PrevPositions: XYPos list
+    BeingDragged: int// create a list of boundingboxes
     }
 
 type Model = {
@@ -34,7 +35,6 @@ type Model = {
     WX: Wire list
     Color: CommonTypes.HighLightColor
     Countselected: int // Capitalize start of words
-    Portlst: ConnectPoint list
     PortToCursor : XYPos * XYPos * Symbol.Port option
     }
 
@@ -49,7 +49,9 @@ type Msg =
     | DeleteWire
     | UpdatedPortsToBusWire of sId : Symbol.Port list
     | DeselectWire
+    | StartDraggingWire of CommonTypes.ConnectionId*XYPos
     | DraggingWire of CommonTypes.ConnectionId*XYPos
+    | EndDraggingWire of CommonTypes.ConnectionId
     | DuplicateWire of XYPos *Symbol.Port list
     | RemoveDrawnLine
     | SelectWiresWithinRegion of Helpers.BoundingBox
@@ -84,6 +86,7 @@ let selectBoundedWires (wModel: Model) (boundary: BoundingBox) =
             {wr with Selected = true}
         else wr
     List.map selectWireinBounds wModel.WX
+
 let drawLineToCursor (startPos : XYPos, endPos : XYPos, endPort : Symbol.Port option) = 
 
     let drawLineColor,strokeWidth,strokeDashArray = 
@@ -116,19 +119,8 @@ let vertexlstZero (cable: Wire) =
     let endpt = cable.TargetSymbol.Pos
     let midX = (startpt.X+endpt.X)/2.0
     let midY = (startpt.Y+endpt.Y)/2.0
-    if (cable.SrcSymbol.PortType=CommonTypes.Input && cable.TargetSymbol.PortType=CommonTypes.Input) then
-        if (startpt.X < endpt.X) then
-            [startpt ; {X=startpt.X-20.0;Y=startpt.Y} ; {X=startpt.X-20.0;Y=endpt.Y} ; endpt]
-        else 
-            [startpt ; {X=endpt.X-20.0;Y=startpt.Y} ; {X=endpt.X-20.0;Y=endpt.Y} ; endpt]
-
-    else if (cable.SrcSymbol.PortType=CommonTypes.Output && cable.TargetSymbol.PortType=CommonTypes.Output) then
-        if (startpt.X > endpt.X) then
-            [startpt ; {X=startpt.X+20.0;Y=startpt.Y} ; {X=startpt.X+20.0;Y=endpt.Y} ; endpt]
-        else 
-            [startpt ; {X=endpt.X+20.0;Y=startpt.Y} ; {X=endpt.X+20.0;Y=endpt.Y} ; endpt]
-
-    else if (cable.SrcSymbol.PortType=CommonTypes.Input && cable.TargetSymbol.PortType=CommonTypes.Output) then
+    
+    if (cable.SrcSymbol.PortType=CommonTypes.Input && cable.TargetSymbol.PortType=CommonTypes.Output) then
         if (startpt.X > endpt.X) then
             [startpt ; {X=midX;Y=startpt.Y} ; {X=midX;Y=endpt.Y} ; endpt]
         else 
@@ -145,25 +137,14 @@ let vertexlstZero (cable: Wire) =
             [startpt ; {X=startpt.X+20.0;Y=startpt.Y} ; {X=startpt.X+20.0;Y=startpt.Y+100.0} ; {X=endpt.X-20.0;Y=startpt.Y+100.0} ; 
             {X=endpt.X-20.0;Y=endpt.Y} ; endpt]
 
-let vertexlist (cable: Wire) = 
+let vertexlist (cable: Wire) (wModel: Model) = 
     let [a ; b ; c] = cable.relativPositions
     let startpt = cable.SrcSymbol.Pos 
     let endpt = cable.TargetSymbol.Pos
     let midX = (startpt.X+endpt.X)/2.0
     let midY = (startpt.Y+endpt.Y)/2.0
-    if (cable.SrcSymbol.PortType=CommonTypes.Input && cable.TargetSymbol.PortType=CommonTypes.Input) then
-        if (startpt.X < endpt.X) then
-            [startpt ; {X=startpt.X-20.0+a.X;Y=startpt.Y} ; {X=startpt.X-20.0+a.X;Y=endpt.Y} ; endpt]
-        else 
-            [startpt ; {X=endpt.X-20.0+a.X;Y=startpt.Y} ; {X=endpt.X-20.0+a.X;Y=endpt.Y} ; endpt]
-
-    else if (cable.SrcSymbol.PortType=CommonTypes.Output && cable.TargetSymbol.PortType=CommonTypes.Output) then
-        if (startpt.X > endpt.X) then
-            [startpt ; {X=startpt.X+20.0+a.X;Y=startpt.Y} ; {X=startpt.X+20.0+a.X;Y=endpt.Y} ; endpt]
-        else 
-            [startpt ; {X=endpt.X+20.0+a.X;Y=startpt.Y} ; {X=endpt.X+20.0+a.X;Y=endpt.Y} ; endpt]
-
-    else if (cable.SrcSymbol.PortType=CommonTypes.Input && cable.TargetSymbol.PortType=CommonTypes.Output) then
+    
+    if (cable.SrcSymbol.PortType=CommonTypes.Input && cable.TargetSymbol.PortType=CommonTypes.Output) then
         if (startpt.X > endpt.X) then
             [startpt ; {X=midX+a.X;Y=startpt.Y} ; {X=midX+a.X;Y=endpt.Y} ; endpt]
         else 
@@ -180,59 +161,49 @@ let vertexlist (cable: Wire) =
             [startpt ; {X=startpt.X+20.0+a.X;Y=startpt.Y} ; {X=startpt.X+20.0+a.X;Y=startpt.Y+100.0+b.Y} ; {X=endpt.X-20.0+c.X;Y=startpt.Y+100.0+b.Y} ; 
             {X=endpt.X-20.0+c.X;Y=endpt.Y} ; endpt]
 
-let individualBound (start:XYPos) (final: XYPos) =
+let bounds (cable: Wire) (wModel: Model)=
+    let individualBound (start:XYPos) (final: XYPos) =
         if (final.Y > start.Y || final.X > start.X) then
             {TopLeft = {X=start.X-15.0;Y=start.Y-15.0} ;
              BottomRight = {X=final.X+15.0;Y=final.Y+15.0}}
          else {TopLeft = {X=final.X-15.0;Y=final.Y-15.0} ;
              BottomRight = {X=start.X+15.0;Y=start.Y+15.0}}
-
-let bounds (cable: Wire) =
-    let vlst = vertexlist cable
+    let vlst = vertexlist cable wModel
     let startlst , endlst = pairListElements vlst
     List.map2 individualBound startlst endlst
 
-let displayFullWire (cable: Wire) (props: WireRenderProps) (vertices: list<XYPos>) = 
-    let displayWireSegment (start: XYPos) (final: XYPos) =
-        let boundary = individualBound start final
-        let topLeft = boundary.TopLeft
-        let bottomRight = boundary.BottomRight
-        let topRight = {X=bottomRight.X;Y=topLeft.Y}
-        let bottomLeft = {X=topLeft.X;Y=bottomRight.Y}
-        let color =
+let autosingleWireView (wModel: Model)= 
+    let displayFullWire (cable: Wire) (props: WireRenderProps) (vertices: list<XYPos>) = 
+        let displayWireSegment (start: XYPos) (final: XYPos) =
+            let color =
                     if props.WireP.Selected then
                         "green"
                     else
                         props.ColorP
-        g   [ Style [ 
+            g   [ Style [ 
             // the transform here does rotation, scaling, and translation
             // the rotation and scaling happens with TransformOrigin as fixed point first
-                TransformOrigin "0px 50px" // so that rotation is around centre of line
-                Transform (sprintf "translate(%fpx,%fpx) rotate(%ddeg) scale(%f)" 0.0 0.0 0 1.0)
+                    TransformOrigin "0px 50px" // so that rotation is around centre of line
+                    Transform (sprintf "translate(%fpx,%fpx) rotate(%ddeg) scale(%f)" 0.0 0.0 0 1.0)
+                    ]
                 ]
-            ]
-            [    
-                line [
-                    X1 start.X
-                    Y1 start.Y
-                    X2 final.X
-                    Y2 final.Y
-                    SVGAttr.Stroke color
-                    SVGAttr.StrokeWidth props.StrokeWidthP
-            ] []
-            ]
+                [    
+                    line [
+                        X1 start.X
+                        Y1 start.Y
+                        X2 final.X
+                        Y2 final.Y
+                        SVGAttr.Stroke color
+                        SVGAttr.StrokeWidth props.StrokeWidthP
+                ] []
+                ]
 
-    let startlst,endlst = pairListElements vertices
-    let reactlst = List.map2 displayWireSegment startlst endlst 
+        let startlst,endlst = pairListElements vertices
+        let reactlst = List.map2 displayWireSegment startlst endlst 
     
-    reactlst
-    |> (fun lineEl -> 
-        g [] lineEl)
+        reactlst |> (fun lineEl -> 
+            g [] lineEl)
 
-let createpoint (comp: Symbol.Symbol) =
-    {Centre = comp.Pos ; Id = comp.Id ; Select = false}
-
-let autosingleWireView = 
     FunctionComponent.Of(
         fun (props: WireRenderProps) ->                                         
             g   [ Style [ 
@@ -241,12 +212,11 @@ let autosingleWireView =
                 ]
                 ]
                 [
-                displayFullWire props.WireP props (vertexlist props.WireP)
+                displayFullWire props.WireP props (vertexlist props.WireP wModel)
                 ]
                 )
 
 let view (model:Model) (dispatch: Msg -> unit)= 
-    
     let wires = 
         model.WX
         |> List.map (fun w ->
@@ -257,7 +227,7 @@ let view (model:Model) (dispatch: Msg -> unit)=
                 TgtP = w.TargetSymbol.Pos
                 ColorP = model.Color.Text()
                 StrokeWidthP = "2px" }
-            autosingleWireView props)
+            autosingleWireView model props)
     let symbols = Symbol.view model.Symbol (fun sMsg -> dispatch (Symbol sMsg))
     let lineToCursor= drawLineToCursor model.PortToCursor
     
@@ -265,19 +235,9 @@ let view (model:Model) (dispatch: Msg -> unit)=
 
 
 let init n () =
-    let symbols, cmd = Symbol.init()
-    let pointlst = List.map createpoint symbols    
-    {WX=[];Symbol=symbols; Color=CommonTypes.Red ; Countselected = 0 ; Portlst = pointlst; PortToCursor = ({X = 0.0; Y= 0.0},{X = 0.0; Y= 0.0}, None)},Cmd.none 
+    let symbols, cmd = Symbol.init()   
+    {WX=[];Symbol=symbols; Color=CommonTypes.Red ; Countselected = 0 ; PortToCursor = ({X = 0.0; Y= 0.0},{X = 0.0; Y= 0.0}, None)},Cmd.none 
 
-let findSeg (cable: Wire) (pos: XYPos) =
-    let inShape (b:BoundingBox) =
-        pos.X > b.TopLeft.X && pos.X < b.BottomRight.X && pos.Y > b.TopLeft.Y && pos.Y < b.BottomRight.Y
-    match List.map inShape (bounds cable) with
-    | [false ; true ; false] -> 0
-    | [false ; true ; false ; false ; false] -> 0
-    | [false ; false ; true ; false ; false] -> 1
-    | [false ; false ; false ; true ; false] -> 2
-    | _ -> -1
 
 let createWire (startPort: Symbol.Port) (endPort: Symbol.Port) (createDU : CreateDU) =
     {
@@ -288,6 +248,7 @@ let createWire (startPort: Symbol.Port) (endPort: Symbol.Port) (createDU : Creat
         BusWidth = 1 ;
         relativPositions = [{X=0.0 ; Y=0.0} ; origin ; origin]
         PrevPositions = [origin ; origin ; origin]
+        BeingDragged = -1
     }
 
 let getWiresToBeDuplicated (displacementPos : XYPos ) (wireList : Wire list) (portList : Symbol.Port list) : (Symbol.Port option *Symbol.Port option) list  =
@@ -305,8 +266,18 @@ let getWiresToBeDuplicated (displacementPos : XYPos ) (wireList : Wire list) (po
                 (sourcePort,endPort)
 
                 )
-
-    
+ 
+let startWireDragging (cable: Wire) (pos: XYPos) (wModel: Model)=
+    let inShape (b:BoundingBox) =
+        pos.X > b.TopLeft.X && pos.X < b.BottomRight.X && pos.Y > b.TopLeft.Y && pos.Y < b.BottomRight.Y
+    let draggedSegment =
+        match List.map inShape (bounds cable wModel) with
+        | [false ; true ; false] -> 0
+        | [false ; true ; false ; false ; false] -> 0
+        | [false ; false ; true ; false ; false] -> 1
+        | [false ; false ; false ; true ; false] -> 2
+        | _ -> -1
+    {cable with BeingDragged = draggedSegment}
 
 let dragAWire (cable: Wire) (pos: XYPos) =
     let vLs = match vertexlstZero cable with
@@ -320,7 +291,7 @@ let dragAWire (cable: Wire) (pos: XYPos) =
     let (d,e,f) = match cable.PrevPositions with
                   | [g ; h ; i] -> g , h , i
                   | _ -> origin , origin , origin 
-    match findSeg cable pos with
+    match cable.BeingDragged with
     | 0 ->  let offset = o
             let correctedPos = posDiff pos offset 
             let diff = posDiff correctedPos d           
@@ -342,7 +313,10 @@ let dragAWire (cable: Wire) (pos: XYPos) =
                 relativPositions = [a ; b ; (posAdd diff) c]
                 PrevPositions = [d ; e ; correctedPos]
             }   
-    |_ -> cable        
+    | _ -> cable      
+
+let endWireDragging (cable: Wire) =
+    {cable with BeingDragged = -1}
 
 let deleteWire (model: Model) (wName: CommonTypes.ConnectionId) =
     List.filter (fun w -> w.Id <> wName) model.WX
@@ -351,10 +325,7 @@ let update (msg : Msg) (model : Model): Model*Cmd<Msg> =
     match msg with
     | Symbol sMsg -> 
         let sm,sCmd = Symbol.update sMsg model.Symbol
-        let followCircle (sm: Symbol.Symbol) cpt =
-            {cpt with Centre = sm.Pos}
-        let nwpts = List.map2 followCircle model.Symbol model.Portlst
-        {model with Symbol=sm ; Portlst = nwpts}, Cmd.map Symbol sCmd
+        {model with Symbol=sm}, Cmd.map Symbol sCmd
     | SetColor c -> {model with Color = c}, Cmd.none
     | SelectWire (wId,p) ->         
         let clickedWire =
@@ -389,6 +360,11 @@ let update (msg : Msg) (model : Model): Model*Cmd<Msg> =
                                {w with Selected = false}
                                ) 
         {model with WX = newWls} , Cmd.none
+    | StartDraggingWire (wid , p) ->
+        let newWls wLs = wLs |> List.map (fun w ->
+                                if w.Id <> wid then w
+                                else startWireDragging w p model)
+        {model with WX = newWls model.WX} , Cmd.none
     | DraggingWire (wid,p) -> 
         let nwWls wLs = wLs |> List.map (fun w ->
                                 if w.Id <> wid then
@@ -440,10 +416,9 @@ let update (msg : Msg) (model : Model): Model*Cmd<Msg> =
 let wireToSelectOpt (wModel: Model) (pos: XYPos) : CommonTypes.ConnectionId option =
     let inShape (b:BoundingBox) =
         pos.X >= b.TopLeft.X && pos.X <= b.BottomRight.X && pos.Y >= b.TopLeft.Y && pos.Y <= b.BottomRight.Y 
-    let blst (cable: Wire) = bounds cable
     let inWireBounds (conId: CommonTypes.ConnectionId) =
         let cable = wire wModel conId 
-        match List.tryFind (inShape) (blst cable) with
+        match List.tryFind (inShape) (bounds cable wModel) with
         |Some b -> true
         |None -> false
     let name cable = cable.Id
