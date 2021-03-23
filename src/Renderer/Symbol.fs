317ed9fa7794855d5ace3244c487eef036e1c37a
kleze20 <68337035+kleze20@users.noreply.github.com>
2021-03-23 22:55:12 +0000

    Update Symbol.fs

--- src/Renderer/Symbol.fs
+++ src/Renderer/Symbol.fs
@@ -771,7 +771,7 @@ let getPortsOf (symModel: Model) (sId: CommonTypes.ComponentId) : Port list =
 let getOrientationOfPort (symModel: Model) (port:Port) : PortOrientation = 
     match getSymbolWithId symModel port.HostId with
     | Some sym -> if (sym.Type=CommonTypes.Mux2 && port.PortNumber=Some 2) then Bottom
-                  else if (sym.Type=CommonTypes.Demux2 && port.PortNumber=Some 2) then Bottom
+                  else if (sym.Type=CommonTypes.Demux2 && port.PortNumber=Some 1) then Bottom
                   else if port.PortType = CommonTypes.PortType.Input then sym.InputOrientation else sym.OutputOrientation
     | _ -> failwithf "The hosting symbol of the given port is not found"
 
